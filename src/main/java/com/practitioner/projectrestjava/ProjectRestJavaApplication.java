71952fa903c59ebb07a31031ff7441e1de2b2199
Geovani Xavier <geovanisantos019@gmail.com>
2023-02-10 16:43:23 -0300

    Adicionando o projeto Spring

---
+++ src/main/java/com/practitioner/projectrestjava/ProjectRestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.practitioner.projectrestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestJavaApplication.class, args);
+	}
+
+}
